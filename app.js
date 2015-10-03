6e014a65257a1ef92f2fb846747717a61ea66939
Alexander Swensen <alexs@videoideas.com>
2015-10-03 18:47:59 -0400

    File IO example

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
+var contents = fs.readFileSync('.gitignore').toString();
+console.log(contents);
+
+fs.readFile('.gitignore', function (err, buf) {
+    console.log(buf.toString());
 });
-
-server.listen(7000);
