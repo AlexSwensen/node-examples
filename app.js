16832c6d94e433ae962322d51d9609d5032f409c
Alexander Swensen <alexs@videoideas.com>
2015-10-03 18:52:54 -0400

    Client/Server example

--- app.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-
-var contents = fs.readFileSync('.gitignore').toString();
-console.log(contents);
-
-fs.readFile('.gitignore', function (err, buf) {
-    console.log(buf.toString());
-});
